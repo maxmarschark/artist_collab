da1fd12d5a243bbd6470d1592e792a3d35c041c7
max marschark <maxmarschark@gmail.com>
2016-11-01 15:25:22 -0400

    ability to find collaborated artists, in console

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -13,6 +13,8 @@ export default class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.searchForArtist = this.searchForArtist.bind(this);
     this.getArtistAlbums = this.getArtistAlbums.bind(this);
+    this.getArtistAlbumTracks = this.getArtistAlbumTracks.bind(this);
+    this.getTrackDetails = this.getTrackDetails.bind(this);
   }
 
   handleChange (e) {
@@ -28,22 +30,28 @@ export default class App extends React.Component {
     this.searchForArtist();
   }
 
-  getArtistInfo () {
-    const id = this.state.selectedArtist.id;
-    request.get(`https://api.spotify.com/v1/artists/${id}`)
+  getArtistAlbums() {
+    const { id } = this.state.selectedArtist;
+    request.get(`https://api.spotify.com/v1/artists/${id}/albums`)
            .then((response) => {
-             console.log(response);
+             response.body.items.forEach(this.getArtistAlbumTracks)
            });
   }
 
-  getArtistAlbums() {
-    const query = this.state.selectAlbums.id;
-    request.get(`https://api.spotify.com/v1/albums/${id}/tracks`)
-           .then((response) => {
-             const album = response.body.albums.items[0];
-             const track = album.track;
-             console.log(response);
-           });
+  getArtistAlbumTracks(album) {
+    request.get(`https://api.spotify.com/v1/albums/${album.id}/tracks`)
+           .then((tracks) => {
+             tracks.body.items.forEach(this.getTrackDetails)
+           })
+  }
+
+  getTrackDetails(track) {
+    setTimeout(function() {
+     request.get(`https://api.spotify.com/v1/tracks/${track.id}`)
+      .then((track) => {
+        console.log(track.body.artists)
+      })
+    }, 1000)
   }
 
   searchForArtist() {
@@ -53,12 +61,17 @@ export default class App extends React.Component {
         const artist = response.body.artists.items[0];
         const name = artist.name;
         const id = artist.id;
+        const img_url = artist.images[0].url;
         this.setState({
-          selectedArtist: {name: name, id: id},
+          selectedArtist: {
+            name,
+            id,
+            img_url,
+          },
         });
       })
       .then(() => {
-        this.getArtistInfo();
+        this.getArtistAlbums();
       })
       .catch((err) => {
         console.error(err);
