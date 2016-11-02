15fdcbf92b6bffc281db1dc405c83ef6394fae8f
max marschark <maxmarschark@gmail.com>
2016-11-02 16:18:12 -0400

    d3 file linked to html and added to src

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -42,46 +42,25 @@ export default class App extends React.Component {
     request.get(`https://api.spotify.com/v1/albums/${album.id}/tracks`)
            .then((tracks) => {
              tracks.body.items.forEach(this.getTrackDetails);
-            //  console.log(this.state.artistCounts); //Sams code
            })
   }
 
   getTrackDetails(track) {
      request.get(`https://api.spotify.com/v1/tracks/${track.id}`)
       .then((track) => {
-        // console.log(track.body.artists);
         let { artists } = track.body;
-        const artistCounts = this.state.artistCounts; // Sams code
+        const artistCounts = this.state.artistCounts;
         for(let i = 0; i < artists.length; i++) {
           let artist = artists[i];
           let artistName = artist.name;
-
-          // console.log(artists[i].name);
-          // console.log(this.state);
-          // if (this.state.artistCounts[artistName]) {
-          if(artistCounts[artistName]) { // Sams IF
-            // console.log(artistName.value);
-            // pull artist out of artistCounts
-            // console.log(artistCounts);
-            // console.log(artist.name);
-            // add 1 to count
-
-
-            // put artist back in state with updated count
-            // this.state.artistCounts[artistName] += 1;
+          if(artistCounts[artistName]) {
             artistCounts[artistName] += 1;
-            // console.log(artist.name);
           }
           else {
-            artistCounts[artistName] = 1; // Sams object update
-            // this.setState({
-            //   artistCounts: {
-            //     [artistName]: 1
-            //   }
-            // })
+            artistCounts[artistName] = 1;
           }
         }
-        this.setState({ artistCounts }); // Sams code
+        this.setState({ artistCounts });
       })
     }
 
@@ -108,6 +87,9 @@ export default class App extends React.Component {
         console.error(err);
       });
   };
+
+  //create a function that loops over this.state.artistCounts
+  //each artist return div with artist name & artist count
 
   render () {
     return (
