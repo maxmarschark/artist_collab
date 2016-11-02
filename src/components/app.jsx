7d37b87f6cba2d9e77f70bf41d7121f287af3de3
max marschark <maxmarschark@gmail.com>
2016-11-02 14:37:08 -0400

    working artist count in  state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -41,42 +41,49 @@ export default class App extends React.Component {
   getArtistAlbumTracks(album) {
     request.get(`https://api.spotify.com/v1/albums/${album.id}/tracks`)
            .then((tracks) => {
-             tracks.body.items.forEach(this.getTrackDetails)
+             tracks.body.items.forEach(this.getTrackDetails);
+            //  console.log(this.state.artistCounts); //Sams code
            })
   }
-// artistCounts = {
-//   'kanye': 1,
-//   'fetty': 2
-// }
+
   getTrackDetails(track) {
      request.get(`https://api.spotify.com/v1/tracks/${track.id}`)
       .then((track) => {
         // console.log(track.body.artists);
         let { artists } = track.body;
+        const artistCounts = this.state.artistCounts; // Sams code
         for(let i = 0; i < artists.length; i++) {
           let artist = artists[i];
           let artistName = artist.name;
 
-          console.log(artists[i].name);
-          if (this.state.artistCounts[artistName]) {
+          // console.log(artists[i].name);
+          // console.log(this.state);
+          // if (this.state.artistCounts[artistName]) {
+          if(artistCounts[artistName]) { // Sams IF
+            // console.log(artistName.value);
             // pull artist out of artistCounts
-            console.log(artistCounts);
-
-
+            // console.log(artistCounts);
+            // console.log(artist.name);
             // add 1 to count
+
+
             // put artist back in state with updated count
-            this.state.artistCounts[artistName] += 1;
+            // this.state.artistCounts[artistName] += 1;
+            artistCounts[artistName] += 1;
+            // console.log(artist.name);
           }
           else {
-            this.setState({
-              artistCounts: {
-                [artistName]: 1
-              }
-            })
+            artistCounts[artistName] = 1; // Sams object update
+            // this.setState({
+            //   artistCounts: {
+            //     [artistName]: 1
+            //   }
+            // })
           }
         }
+        this.setState({ artistCounts }); // Sams code
       })
-  }
+    }
 
   searchForArtist() {
     const query = this.state.searchInput.replace(' ', '%20');
