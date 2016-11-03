a739645a6477cfb767960dc5e5bb4420e8c26564
max marschark <maxmarschark@gmail.com>
2016-11-03 18:04:57 -0400

    logging artistCollab onClick && new artistCollab component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import request from 'superagent';
 
 export default class App extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       searchInput: '',
       selectedArtist: {},
       selectAlbums: {},
-      artistCounts: {}
+      artistCounts: {},
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,16 +16,17 @@ export default class App extends React.Component {
     this.getArtistAlbums = this.getArtistAlbums.bind(this);
     this.getArtistAlbumTracks = this.getArtistAlbumTracks.bind(this);
     this.getTrackDetails = this.getTrackDetails.bind(this);
+    this.getArtistCollabs = this.getArtistCollabs.bind(this);
   }
 
-  handleChange (e) {
+  handleChange(e) {
     e.preventDefault();
     this.setState({
-      searchInput: e.target.value
+      searchInput: e.target.value,
     });
   }
 
-  handleSubmit (e) {
+  handleSubmit(e) {
     e.preventDefault();
     this.searchForArtist();
   }
@@ -34,7 +35,7 @@ export default class App extends React.Component {
     const { id } = this.state.selectedArtist;
     request.get(`https://api.spotify.com/v1/artists/${id}/albums`)
            .then((response) => {
-             response.body.items.forEach(this.getArtistAlbumTracks)
+             response.body.items.forEach(this.getArtistAlbumTracks);
            });
   }
 
@@ -42,18 +43,18 @@ export default class App extends React.Component {
     request.get(`https://api.spotify.com/v1/albums/${album.id}/tracks`)
            .then((tracks) => {
              tracks.body.items.forEach(this.getTrackDetails);
-           })
+           });
   }
 
   getTrackDetails(track) {
-     request.get(`https://api.spotify.com/v1/tracks/${track.id}`)
+    request.get(`https://api.spotify.com/v1/tracks/${track.id}`)
       .then((track) => {
-        let { artists } = track.body;
+        const { artists } = track.body;
         const artistCounts = this.state.artistCounts;
-        for(let i = 0; i < artists.length; i++) {
-          let artist = artists[i];
-          let artistName = artist.name;
-          if(artistCounts[artistName]) {
+        for (let i = 0; i < artists.length; i++) {
+          const artist = artists[i];
+          const artistName = artist.name;
+          if (artistCounts[artistName]) {
             artistCounts[artistName] += 1;
           }
           else {
@@ -61,8 +62,8 @@ export default class App extends React.Component {
           }
         }
         this.setState({ artistCounts });
-      })
-    }
+      });
+  }
 
   searchForArtist() {
     const query = this.state.searchInput.replace(' ', '%20');
@@ -86,23 +87,28 @@ export default class App extends React.Component {
       .catch((err) => {
         console.error(err);
       });
-  };
-
-  onClick (e) {
-    e.preventDefault();
-    this.artistCounts();
   }
 
-  render () {
+  getArtistCollabs() {
+    console.log('reached get artist collab function');
+    const { artistCounts } = this.state;
+    console.log(artistCounts);
+    const artist = Object.keys(artistCounts).map(function(key) {
+      return (
+        <div>{artistCounts[key]}</div>
+      );
+    });
+  }
+
+  render() {
     const img_url = this.state.selectedArtist.img_url;
-    const artistCounts = this.state.selectedArtist.img_url;
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <input type='text' name='searchInput' className="searchInput" placeholder="Artist" onChange={this.handleChange} />
           <input type='submit' className="button" />
         </form>
-          <img className="artist-img" src={this.state.selectedArtist.img_url} alt="" />
+          <img className="artist-img" src={this.state.selectedArtist.img_url} onClick={this.getArtistCollabs.artistCounts} alt="" />
       </div>
 
     );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
+import ArtistCollaborators from './components/artistCollaborators.jsx';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
