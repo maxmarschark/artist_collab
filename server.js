798d099396c0fd84d22233712c43fc11d9a5b7e3
max marschark <maxmarschark@gmail.com>
2016-11-08 12:48:30 -0500

    funcioning server

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+process.env.ENV = process.env.ENV || 'dev';
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
-
-app.use(express.static(path.join(__dirname, '/static')));
+const wpMiddleWare = require('webpack-dev-middleware');
+const wpHotMiddleWare = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+const app = require('./app/app.js');
+if (process.env.ENV === 'dev') {
+  const compiler = webpack(config);
+  const midWare = wpMiddleWare(compiler, {
+    stats: {
+      colors: true,
+      chunks: false,
+    },
+  });
+  app.use(midWare);
+  app.use(wpHotMiddleWare(compiler));
+}
+app.use(express.static(path.join(__dirname, '/src/static')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static/index.html'));
+  res.sendFile(path.join(__dirname, '/src/static/index.html'));
 });
 const port = 3000;
 app.listen(port, () => {
